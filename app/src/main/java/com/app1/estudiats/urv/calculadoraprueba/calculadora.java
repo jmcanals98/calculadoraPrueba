2a609d3c46c7fbd3351ccd357b2c0ff70f3784f3
jmcanals98 <juanmafcb2009@gmail.com>
2019-04-08 09:12:20 +0200

    Initial commit

---
+++ app/src/main/java/com/app1/estudiats/urv/calculadoraprueba/calculadora.java
@@ -0,0 +1,13 @@
+package com.app1.estudiats.urv.calculadoraprueba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class calculadora extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora);
+    }
+}
